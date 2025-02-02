f00fa62fdb44644e79151c2ead902d06af909ac1
ljvmiranda921 <ljvmiranda@gmail.com>
2025-02-02 14:19:09 -0800

    Add NewsPH NLI benchmark to Filbench

--- community_tasks/filbench_evals.py
+++ community_tasks/filbench_evals.py
@@ -30,10 +30,15 @@ This file generally creates just a TASKS_TABLE and TASKS_GROUPS which are then i
 from filbench.belebele import FILIPINO_BELEBELE_TASKS
 from filbench.firecs import FILIPINO_FIRECS_TASK
 from filbench.global_mmlu import FILIPINO_GLOBAL_MMLU_TASKS
+from filbench.newsph_nli import FILIPINO_NEWSPH_NLI_TASKS
 from filbench.sib200 import FILIPINO_SIB_TASKS
 from lighteval.tasks.lighteval_task import LightevalTaskConfig
 
 
 TASKS_TABLE: list[LightevalTaskConfig] = (
-    FILIPINO_GLOBAL_MMLU_TASKS + FILIPINO_FIRECS_TASK + FILIPINO_SIB_TASKS + FILIPINO_BELEBELE_TASKS
+    FILIPINO_GLOBAL_MMLU_TASKS
+    + FILIPINO_FIRECS_TASK
+    + FILIPINO_SIB_TASKS
+    + FILIPINO_BELEBELE_TASKS
+    + FILIPINO_NEWSPH_NLI_TASKS
 )
