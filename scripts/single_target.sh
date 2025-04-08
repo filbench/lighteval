1722b70001309b697d138d056cd8150f1be303c3
ljvmiranda921 <ljvmiranda@gmail.com>
2025-04-07 20:19:27 -0700

    Add single target script

---
+++ scripts/single_target.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+MODEL_NAME=$1
+TASK_NAME=$2
+
+if [ -z "$MODEL_NAME" ]; then
+  echo "Error: No model name provided."
+  echo "Usage: ./single_target.sh <MODEL_NAME> <TASK_NAME>"
+  exit 1
+fi
+
+if [ -z "$TASK_NAME" ]; then
+  echo "Error: No task name provided."
+  echo "Usage: ./single_target.sh <MODEL_NAME> <TASK_NAME>"
+  exit 1
+fi
+
+python -m lighteval accelerate ${MODEL_NAME} ${TASK_NAME} --push-to-hub --results-org UD-Filipino --custom-tasks community_tasks/filbench_evals.py
